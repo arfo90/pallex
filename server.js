442ad9ac842b4cced2fe3137e4b0f89482fb843f
Amireza Fatemi <arfo.amireza@gmail.com>
2017-04-17 18:39:34 +0200

    rendering canvas htmlx

--- server.js
+++ server.js
@@ -8,14 +8,10 @@ var server_host = process.env.YOUR_HOST || '0.0.0.0';
 
 var port = process.env.PORT || 5000;
 
-app.engine('html', require('ejs').renderFile);
-app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/view'));
 
-app.use(express.static(__dirname + '/'));
-
-
-app.get('/', function(req, res) {
-            res.render('index');
+app.get('/',function(req,res){
+  res.sendFile(path.join(__dirname+'canvas.html'));
 });
 
 
@@ -24,7 +20,7 @@ console.log('Here we go..');
 console.log('Server is Started');
 
 
-app.listen(server_port, server_host, function() {
-        console.log('Listening on port %d', server_port);
-            console.log('Server host %d', server_host);
+app.listen(server_port, server_host, function () {
+    console.log('Listening on port %d', server_port);
+    console.log('Server host %d', server_host);
 });
